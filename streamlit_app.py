c8644b915310cc845a346e9050ab05ebadbf21d6
Fauzi4232401023 <Ahmad53785s@gmail.com>
2025-12-23 16:34:09 +0700

    Menghapus yang double

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,8 +109,3 @@ values = st.slider(
     label='Select a range of values',
     min_value=0, max_value=100, value=(0, 100))
 st.write('Values:', values)
-
-values = st.slider(
-    label='Select a range of values',
-    min_value=0, max_value=100, value=(0, 100))
-st.write('Values:', values)
