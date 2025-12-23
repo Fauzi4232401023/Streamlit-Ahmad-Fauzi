21d497cb114e6c58c78c9c0281c78606bc55902e
Fauzi4232401023 <Ahmad53785s@gmail.com>
2025-12-23 16:13:31 +0700

    jj

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,3 +119,8 @@ values = st.slider(
     label='Select a range of values',
     min_value=0, max_value=100, value=(0, 100))
 st.write('Values:', values)
+
+values = st.slider(
+    label='Select a range of values',
+    min_value=0, max_value=100, value=(0, 100))
+st.write('Values:', values)
